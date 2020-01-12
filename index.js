3d44abd5d57a24520eb814042e4e0554c111ec0f
Daniel Le'Sage <DLesage25@users.noreply.github.com>
2020-01-12 12:35:55 -0600

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const files = require('./lib/files');
+const form = require('./lib/form');
+
+const github = require('./lib/github');
+
+clear();
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('gitneat', {
+            font: 'Kban',
+            horizontalLayout: 'full'
+        })
+    )
+);
+
+// if (files.directoryExists('.git')) {
+//     console.log(chalk.red('Already a Git repository. Exiting...'));
+//     process.exit();
+// }
+
+const main = async () => {
+    await github.appAuthentication();
+    // let token = github.getStoredGithubToken();
+    // if (!token) {
+    //     await github.setGithubCredentials();
+    //     token = await github.registerNewToken();
+    // }
+    // console.log(token);
+};
+
+main();
